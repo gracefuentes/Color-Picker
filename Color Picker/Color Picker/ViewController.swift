ee1ba46e52125dd8fa4def932e459842d3a41ad9
sheyla fuentes <sgf763@mail.missouri.edu>
2018-04-19 22:37:37 -0500

    first version

---
+++ Color Picker/Color Picker/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Color Picker
+//
+//  Created by Sheyla Astete on 4/19/18.
+//  Copyright © 2018 tech innovator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController ,UIPickerViewDataSource, UIPickerViewDelegate{
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    
+    let colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+        }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return colors[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return colors.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        label.text = colors[row]
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
